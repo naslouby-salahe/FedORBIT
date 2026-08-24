eb1f83805a6a34102b21b36cf1e27fe660425c93
Salaheddine Naslouby <naslouby.salahe@gmail.com>
2026-08-24 12:35:52 +0100

    Use library type information

--- stubs/scipy/__init__.pyi
+++
@@ -1,1 +0,0 @@
-from . import optimize as optimize
--- stubs/torch/nn/utils/__init__.pyi
+++
@@ -1,7 +0,0 @@
-from typing import Iterator
-
-from torch import Tensor
-from torch.nn import Parameter
-
-
-def clip_grad_norm_(parameters: Iterator[Parameter], max_norm: float) -> Tensor: ...

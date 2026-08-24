bfacfaf352e8cb33d543623ecfb3374509bb2e41
Salaheddine Naslouby <naslouby.salahe@gmail.com>
2026-08-24 12:35:52 +0100

    Use library type information

--- stubs/torch/backends/cuda/__init__.pyi
+++
@@ -1,8 +0,0 @@
-class matmul:
-    allow_tf32: bool
-    fp32_precision: str
-    stochastic_rounding: bool
-
-
-def is_available() -> bool: ...
-def get_device_name(device: object | None = None) -> str: ...

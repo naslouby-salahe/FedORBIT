f6a90e36dd66e3142f7696bf4d06f5b9a7d64456
Salaheddine Naslouby <naslouby.salahe@gmail.com>
2026-08-27 23:55:11 +0100

    Repair quality gates and typing

--- stubs/scipy/optimize/__init__.pyi
+++ stubs/scipy/optimize/__init__.pyi
@@ -3,7 +3,6 @@ from __future__ import annotations
 import numpy as np
 from numpy.typing import NDArray
 
-
 def linear_sum_assignment(
     cost_matrix: NDArray[np.float64],
     maximize: bool = False,

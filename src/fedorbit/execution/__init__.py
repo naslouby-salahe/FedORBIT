314b2dbf787f603a7f0807da87bbd17583d7c448
Salaheddine Naslouby <naslouby.salahe@gmail.com>
2026-08-29 23:36:40 +0100

    refactor: expose raw inventory execution surface

---
+++ src/fedorbit/execution/__init__.py
@@ -0,0 +1,15 @@
+from fedorbit.execution.inventory import (
+    RawDatasetInventory,
+    RawFileInventory,
+    RawInventoryError,
+    RawInventoryRequest,
+    inspect_raw_inventory,
+)
+
+__all__ = [
+    "RawDatasetInventory",
+    "RawFileInventory",
+    "RawInventoryError",
+    "RawInventoryRequest",
+    "inspect_raw_inventory",
+]
